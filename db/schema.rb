153b7be965b589e3ae0129ade921391f8cd0c4b0
Jose Villalobos <lvilllaobos@gmail.com>
2018-11-14 11:23:22 -0600

    testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_062405) do
+ActiveRecord::Schema.define(version: 2018_11_14_170230) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
